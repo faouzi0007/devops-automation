567155ece090609b649882202bdc189385a1a199
faouzi0007 <faouziseddouki49@gmail.com>
2022-08-12 18:29:37 +0100

    Faouzi Comment

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to faouzi seddouki SpringBoot";
+		return "welcome to faouzi seddouki SpringBoot New";
 	}
 
 	public static void main(String[] args) {
