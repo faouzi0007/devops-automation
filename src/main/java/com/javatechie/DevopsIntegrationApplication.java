f04cf16c468b0c12e6d0a7060c62e232b3a3a26c
faouzi0007 <faouziseddouki49@gmail.com>
2022-08-15 10:12:34 +0100

    New Comment from FAouzi

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to faouzi seddouki SpringBoot New";
+		return "Salamo 3alaykom";
 	}
 
 	public static void main(String[] args) {
